2a8d7528edec7d2b858f30e8dcd248e29e67bcd5
alejandra cantu <alejandra.cantu13@gmail.com>
2019-08-06 01:33:34 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -41,7 +41,7 @@ attr_accessor :sender, :receiver, :amount, :status
         return "Transaction rejected. Please check your account balance."
       elsif @sender.valid? == false && @status = "complete"
         @sender.balance = @sender.balance + @amount
-        @receiver.balance= @receiver.balance - @amount
+        @receiver.balance = @receiver.balance - @amount
       end
     end
 
