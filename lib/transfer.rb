470ce5731e7ccc9ae205818f0f0388609f4a382c
alejandra cantu <alejandra.cantu13@gmail.com>
2019-08-06 01:32:18 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -29,7 +29,7 @@ attr_accessor :sender, :receiver, :amount, :status
         @sender.balance = @sender.balance - @amount
         @receiver.balance = @receiver.balance + @amount
         @status = "complete"
-    
+
       end
     end
 
