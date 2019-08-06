2816c592485913a5683081c609670b6596205897
alejandra cantu <alejandra.cantu13@gmail.com>
2019-08-06 01:32:16 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -23,13 +23,13 @@ attr_accessor :sender, :receiver, :amount, :status
     def execute_transaction
 
     #  binding.pry
-
-      if @sender.valid? == true && @status != "complete"
+      if @sender.balance < @amount
+        return "Transaction rejected. Please check your account balance."
+      elsif @sender.valid? == true && @status != "complete"
         @sender.balance = @sender.balance - @amount
         @receiver.balance = @receiver.balance + @amount
         @status = "complete"
-      elsif @sender.balance < @amount
-        return "Transaction rejected. Please check your account balance."
+    
       end
     end
 
