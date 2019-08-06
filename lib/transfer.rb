1d05ba23eb044e1fb2ad7f7ddb59f1aa7781bb2b
alejandra cantu <alejandra.cantu13@gmail.com>
2019-08-06 01:22:57 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -21,9 +21,9 @@ attr_accessor :sender, :receiver, :amount, :status
     end
 
     def execute_transaction
-      binding.pry
+      #binding.pry
 
-      if @sender.valid? == true && @status = "pending"
+      if @sender.valid? == true && @status != "complete"
         @sender.balance = sender.balance - @amount
         @receiver.balance = receiver.balance + @amount
         @status = "complete"
